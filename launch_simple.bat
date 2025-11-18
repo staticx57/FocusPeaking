57507fd2f6e24e0cf1ecad7ec6de9cec96a215c9
staticx57 <46908954+staticx57@users.noreply.github.com>
2025-11-18 10:41:53 -0500

    Updates

--- launch_simple.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Quick Launch - Unified Application
-REM NOTE: Simple/Advanced distinction removed - all features now in one application
-echo Starting FLIR Boson Focus Utility v3.0.1...
-python focus_utility.py
